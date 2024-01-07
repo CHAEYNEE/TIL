4249ea8725a97f9cc7c27b5561b92293b30991e7
CHAEYEON <142197244+CHAEYNEE@users.noreply.github.com>
2024-01-07 11:35:28 +0900

    20240107 : shoppingPjt

---
+++ JAVA/Eclipse/shoppingPjt/src/chaeyeon/chmarket/run/Start.java
@@ -0,0 +1,12 @@
+package chaeyeon.chmarket.run;
+
+import chaeyeon.chmarket.vo.Goods;
+
+public class Start {
+
+	public static void main(String[] args) {
+		Goods g = new Goods();
+		g.printList();
+	}
+
+}
---
+++ JAVA/Eclipse/shoppingPjt/src/chaeyeon/chmarket/vo/Goods.java
@@ -0,0 +1,7 @@
+package chaeyeon.chmarket.vo;
+
+public class Goods {
+	public void printList() {
+		System.out.println("맥북프로 2023-16인치 / 300만원");
+	}
+}
